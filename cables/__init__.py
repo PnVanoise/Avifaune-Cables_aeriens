3d2448b9b042f7902a6a0d07c35efe6022b5da92
Antoine Abt <antoine@abt.im>
2018-03-01 10:58:10 +0100

    Add app skeleton

---
+++ cables/__init__.py
@@ -0,0 +1,46 @@
+#-*- coding: utf-8 -*-
+import os
+
+from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.decorator import reify
+from pyramid.request import Request
+from pyramid.security import unauthenticated_userid
+import sqlalchemy
+import sqlahelper
+import pyramid_tm
+import papyrus
+
+from pyramid_beaker import session_factory_from_settings
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+
+    session_factory = session_factory_from_settings(settings)
+
+    config = Configurator(root_factory=Root, settings=settings,
+                          session_factory=session_factory)
+
+    config.include('pyramid_mako')
+
+    # initialize database
+    engine = sqlalchemy.engine_from_config(settings, 'sqlalchemy.')
+    sqlahelper.add_engine(engine)
+
+
+    # add the "geojson" and "csv" renderer
+    config.add_renderer(name='csv', factory='siacg62.renderers.CSVRenderer')
+
+    # add routes to the entry view class
+    config.add_route('home', '/')
+
+    # we need to call scan() for the "home" and "countries"
+    # routes
+    config.scan()
+
+    # add the static view (for static resources)
+    config.add_static_view('static', 'siacg62:static')
+
+    return config.make_wsgi_app()
+
---
+++ setup.py
@@ -0,0 +1,65 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid_mako',
+    'pyramid',
+    'psycopg2',
+    'SQLAlchemy',
+    'SQLAHelper',
+    'pyramid_tm',
+    'PasteScript',
+    'pyramid_debugtoolbar',
+    'ordereddict',
+    'PyYAML',
+    'pyramid_beaker',
+    'GeoAlchemy2',
+    'backports.shutil_get_terminal_size',
+    'pexpect',
+    'pathlib2',
+    'six',
+    'traitlets',
+    'enum',
+    'pgcli',
+]
+
+test_requires = requires.extend(['WebTest',
+                                 'pyquery',
+                                 'mock',
+                                 'nose>=1.2.1',
+                                 'nosexcover',
+                                 'nose-progressive',
+                                 'coverage',
+                                 'nose-testconfig',
+                                 'BeautifulSoup'])
+
+setup(name='Cables',
+      version='0.0',
+      description='Cables',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      test_suite="cables",
+      entry_points="""\
+      [paste.app_factory]
+      main = cables:main
+      """,
+      )
