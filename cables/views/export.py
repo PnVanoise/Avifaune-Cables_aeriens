65d23917ffe6b1fb63feb69ae73d2c2efd3e159d
Antoine Abt <antoine@abt.im>
2018-03-05 11:10:19 +0100

    Add nb poteaux with static years

--- cables/views/export.py
+++ cables/views/export.py
@@ -3,13 +3,14 @@ import logging
 
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound, HTTPBadRequest
-from pyramid.security import remember, forget
-from pyramid.response import Response
+
+from sqlalchemy import extract
+from numpy import array
 
 from cables.models import DBSession
-# from cables.models import TZonesSensible
 from cables.models import TVZonesSensibles
-from numpy import array
+from cables.models import TInventairePoteauxErdf
+from cables.models import TEquipementsPoteauxErdf
 
 
 log = logging.getLogger(__name__)
@@ -17,27 +18,52 @@ log = logging.getLogger(__name__)
 @view_config(route_name='export', renderer='csv')
 def export(request):
     rows = map(to_dict, DBSession.query(TVZonesSensibles).all())
+    rows.insert(0, (
+        'id',
+        'nom',
+        'Nb poteaux risque fort',
+        'Nb poteaux risque secondaire',
+        'Nb poteaux risque',
+        'Nb poteaux équipés risque fort',
+        'Nb poteaux équipés risque secondaire',
+        'Nb poteaux équipés risque',
+        'Nb poteaux équipés en 2014',
+        'Nb poteaux équipés en 2015',
+        'Nb poteaux équipés en 2016',
+        'Longueur troncons risque élevé',
+        'Longueur troncons risque secondaire',
+        'Longueur troncons risque',
+        'Longueur troncons équipés risque élevé',
+        'Longueur troncons équipés risque secondaire',
+        'Longueur troncons équipés risque',
+        ))
     return array(rows).transpose()
 
 def to_dict(item):
     return (
         item.id_zone_sensible,
         item.nom_zone_sensible,
-        item.niveau_sensibilite,
-        item.nb_poteaux_inventories,
         item.nb_poteaux_inventories_risque_fort,
         item.nb_poteaux_inventories_risque_secondaire,
-        item.nb_poteaux_inventories_risque_faible,
-        item.nb_poteaux_equipes,
+        (item.nb_poteaux_inventories_risque_fort or 0) + (item.nb_poteaux_inventories_risque_secondaire or 0),
         item.nb_poteaux_equipes_risque_fort,
         item.nb_poteaux_equipes_risque_secondaire,
-        item.nb_poteaux_equipes_risque_faible,
-        item.m_troncons_inventories,
+        (item.nb_poteaux_equipes_risque_fort or 0) + (item.nb_poteaux_equipes_risque_secondaire or 0),
+        get_nb_poteaux(item, 2014),
+        get_nb_poteaux(item, 2015),
+        get_nb_poteaux(item, 2016),
         item.m_troncons_inventories_risque_fort,
         item.m_troncons_inventories_risque_secondaire,
-        item.m_troncons_inventories_risque_faible,
-        item.m_troncons_equipes,
+        (item.m_troncons_inventories_risque_fort or 0) + (item.m_troncons_inventories_risque_secondaire or 0),
         item.m_troncons_equipes_risque_fort,
         item.m_troncons_equipes_risque_secondaire,
-        item.m_troncons_equipes_risque_faible,
+        (item.m_troncons_equipes_risque_fort or 0) + (item.m_troncons_equipes_risque_secondaire or 0),
         )
+
+def get_nb_poteaux(item, year):
+    year_extract = extract('year', TEquipementsPoteauxErdf.date_equipement)
+    return DBSession.query(TInventairePoteauxErdf).\
+            join(TEquipementsPoteauxErdf).\
+            filter(TInventairePoteauxErdf.id_zone_sensible==item.id_zone_sensible).\
+            filter(year_extract==year).\
+            count()
