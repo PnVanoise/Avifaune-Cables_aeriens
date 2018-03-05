49d3b825a344d53c322112d86eae1b5244dda78c
Antoine Abt <antoine@abt.im>
2018-03-05 15:16:21 +0100

    Add troncon length with static years

--- cables/views/export.py
+++ cables/views/export.py
@@ -5,13 +5,13 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound, HTTPBadRequest
 
 from sqlalchemy import extract
+from sqlalchemy.sql import func
 from numpy import array
 
 from cables.models import DBSession
-from cables.models import TVZonesSensibles
-from cables.models import TInventairePoteauxErdf
-from cables.models import TEquipementsPoteauxErdf
-
+from cables.models import TVZonesSensibles, \
+        TInventairePoteauxErdf, TEquipementsPoteauxErdf, \
+        TInventaireTronconsErdf, TEquipementsTronconsErdf
 
 log = logging.getLogger(__name__)
 
@@ -41,6 +41,9 @@ def export_zonessensibles(request):
         'Longueur troncons équipés risque élevé',
         'Longueur troncons équipés risque secondaire',
         'Longueur troncons équipés risque',
+        'Longueur troncons équipés en 2014',
+        'Longueur troncons équipés en 2015',
+        'Longueur troncons équipés en 2016',
         ))
     return array(entries).transpose()
 
@@ -63,6 +66,9 @@ def to_dict(item):
         item.m_troncons_equipes_risque_fort,
         item.m_troncons_equipes_risque_secondaire,
         (item.m_troncons_equipes_risque_fort or 0) + (item.m_troncons_equipes_risque_secondaire or 0),
+        get_len_troncons(item, 2014),
+        get_len_troncons(item, 2015),
+        get_len_troncons(item, 2016),
         )
 
 def get_nb_poteaux(item, year):
@@ -72,3 +78,12 @@ def get_nb_poteaux(item, year):
             filter(TInventairePoteauxErdf.id_zone_sensible==item.id_zone_sensible).\
             filter(year_extract==year).\
             count()
+
+def get_len_troncons(item, year):
+    year_extract = extract('year', TEquipementsTronconsErdf.date_equipement_troncon)
+    length = DBSession.query(func.sum(TInventaireTronconsErdf.lg_equipee)).\
+            join(TEquipementsTronconsErdf).\
+            filter(TInventaireTronconsErdf.id_zone_sensible==item.id_zone_sensible).\
+            filter(year_extract==year).\
+            first()[0]
+    return 0 if length is None else float(length)
