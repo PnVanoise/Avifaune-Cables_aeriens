da86f7dbffafaeb8cd40620bed0a4e61522ccb38
Antoine Abt <antoine@abt.im>
2018-03-08 13:38:10 +0100

    Extract year list from data

--- cables/views/export.py
+++ cables/views/export.py
@@ -15,54 +15,58 @@ from cables.models import TVZonesSensibles, \
 
 log = logging.getLogger(__name__)
 
+year_extract_p = extract('year', TEquipementsPoteauxErdf.date_equipement)
+year_extract_t = extract('year', TEquipementsTronconsErdf.date_equipement_troncon)
+years_p = ()
+years_t = ()
+
 @view_config(route_name='export_zonessensibles', renderer='csv')
 def export_zonessensibles(request):
+    global years_p, years_t
     query = DBSession.query(TVZonesSensibles)
     if request.params.has_key('ids'):
         ids = map(int, request.params.get('ids').split(','))
         query = query.filter(TVZonesSensibles.id_zone_sensible.in_(ids))
     rows = query.all()
+    to_int = lambda x: int(x[0])
+    years_p = tuple(sorted(set(map(to_int, DBSession.query(year_extract_p).all()))))
+    years_t = tuple(sorted(set(map(to_int, DBSession.query(year_extract_t).all()))))
     entries = map(zs_to_dict, rows)
     add_header_row(entries, 'nom')
     return array(entries).transpose()
 
 def zs_to_dict(item):
-    return (
+    poteaux = (
         item.nom_zone_sensible,
         item.nb_poteaux_inventories_risque_fort,
         item.nb_poteaux_inventories_risque_secondaire,
         (item.nb_poteaux_inventories_risque_fort or 0) + (item.nb_poteaux_inventories_risque_secondaire or 0),
         item.nb_poteaux_equipes_risque_fort,
         item.nb_poteaux_equipes_risque_secondaire,
-        (item.nb_poteaux_equipes_risque_fort or 0) + (item.nb_poteaux_equipes_risque_secondaire or 0),
-        get_nb_poteaux(item, 2014),
-        get_nb_poteaux(item, 2015),
-        get_nb_poteaux(item, 2016),
+        (item.nb_poteaux_equipes_risque_fort or 0) + (item.nb_poteaux_equipes_risque_secondaire or 0))
+    poteaux_year = tuple( get_nb_poteaux(item, year) for year in years_p )
+    troncons = (
         item.m_troncons_inventories_risque_fort,
         item.m_troncons_inventories_risque_secondaire,
         (item.m_troncons_inventories_risque_fort or 0) + (item.m_troncons_inventories_risque_secondaire or 0),
         item.m_troncons_equipes_risque_fort,
         item.m_troncons_equipes_risque_secondaire,
-        (item.m_troncons_equipes_risque_fort or 0) + (item.m_troncons_equipes_risque_secondaire or 0),
-        get_len_troncons(item, 2014),
-        get_len_troncons(item, 2015),
-        get_len_troncons(item, 2016),
-        )
+        (item.m_troncons_equipes_risque_fort or 0) + (item.m_troncons_equipes_risque_secondaire or 0))
+    troncons_year = tuple( get_len_troncons(item, year) for year in years_t )
+    return poteaux + poteaux_year + troncons + troncons_year
 
 def get_nb_poteaux(item, year):
-    year_extract = extract('year', TEquipementsPoteauxErdf.date_equipement)
     return DBSession.query(TInventairePoteauxErdf).\
             join(TEquipementsPoteauxErdf).\
             filter(TInventairePoteauxErdf.id_zone_sensible==item.id_zone_sensible).\
-            filter(year_extract==year).\
+            filter(year_extract_p==year).\
             count()
 
 def get_len_troncons(item, year):
-    year_extract = extract('year', TEquipementsTronconsErdf.date_equipement_troncon)
     length = DBSession.query(func.sum(TInventaireTronconsErdf.lg_equipee)).\
             join(TEquipementsTronconsErdf).\
             filter(TInventaireTronconsErdf.id_zone_sensible==item.id_zone_sensible).\
-            filter(year_extract==year).\
+            filter(year_extract_t==year).\
             first()[0]
     return 0 if length is None else float(length)
 
@@ -101,6 +105,8 @@ def commune_to_dict(item):
         )
 
 def add_header_row(entries, name):
+    labels_years_p = tuple('Nb poteaux équipés en %s' % year for year in years_p)
+    labels_years_t = tuple('Nb troncons équipés en %s' % year for year in years_t)
     entries.insert(0, (
         name,
         'Nb poteaux risque fort',
@@ -108,17 +114,13 @@ def add_header_row(entries, name):
         'Nb poteaux risque',
         'Nb poteaux équipés risque fort',
         'Nb poteaux équipés risque secondaire',
-        'Nb poteaux équipés risque',
-        'Nb poteaux équipés en 2014',
-        'Nb poteaux équipés en 2015',
-        'Nb poteaux équipés en 2016',
+        'Nb poteaux équipés risque') +
+        labels_years_p + (
         'Longueur troncons risque élevé',
         'Longueur troncons risque secondaire',
         'Longueur troncons risque',
         'Longueur troncons équipés risque élevé',
         'Longueur troncons équipés risque secondaire',
-        'Longueur troncons équipés risque',
-        'Longueur troncons équipés en 2014',
-        'Longueur troncons équipés en 2015',
-        'Longueur troncons équipés en 2016',
-        ))
+        'Longueur troncons équipés risque') +
+        labels_years_t
+        )
