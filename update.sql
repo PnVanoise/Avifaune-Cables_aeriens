04d216e326049789e3a0fdf07a918f5bb9c55643
Antoine Abt <antoine@abt.im>
2018-04-30 16:53:30 +0200

    Fix cables74 schema

--- update.sql
+++ update.sql
@@ -1,3 +1,6 @@
+
+-- schema cables73
+
 ALTER TABLE cables73.t_inventaire_poteaux_erdf ADD COLUMN insee INTEGER;
 ALTER TABLE cables73.t_inventaire_poteaux_erdf ADD CONSTRAINT t_inventaire_poteaux_erdf_insee_fk FOREIGN KEY (insee) REFERENCES cables73.t_communes(insee);
 UPDATE cables73.t_inventaire_poteaux_erdf SET insee = c.insee FROM cables73.t_inventaire_poteaux_erdf p JOIN cables73.t_communes c ON p.commune=c.nom_commune;
@@ -5,3 +8,15 @@ UPDATE cables73.t_inventaire_poteaux_erdf SET insee = c.insee FROM cables73.t_in
 ALTER TABLE cables73.t_inventaire_troncons_erdf ADD COLUMN insee INTEGER;
 ALTER TABLE cables73.t_inventaire_troncons_erdf ADD CONSTRAINT t_inventaire_troncons_erdf_insee_fk FOREIGN KEY (insee) REFERENCES cables73.t_communes(insee);
 UPDATE cables73.t_inventaire_troncons_erdf SET insee = c.insee FROM cables73.t_inventaire_troncons_erdf p JOIN cables73.t_communes c ON p.commune=c.nom_commune;
+
+
+-- schema cables74
+ALTER TABLE cables74.t_communes ADD CONSTRAINT t_communes_pk PRIMARY KEY (insee);
+
+ALTER TABLE cables74.t_inventaire_poteaux_erdf ADD COLUMN insee INTEGER;
+ALTER TABLE cables74.t_inventaire_poteaux_erdf ADD CONSTRAINT t_inventaire_poteaux_erdf_insee_fk FOREIGN KEY (insee) REFERENCES cables74.t_communes(insee);
+UPDATE cables74.t_inventaire_poteaux_erdf SET insee = c.insee FROM cables74.t_inventaire_poteaux_erdf p JOIN cables74.t_communes c ON p.commune=c.nom_commune;
+
+ALTER TABLE cables74.t_inventaire_troncons_erdf ADD COLUMN insee INTEGER;
+ALTER TABLE cables74.t_inventaire_troncons_erdf ADD CONSTRAINT t_inventaire_troncons_erdf_insee_fk FOREIGN KEY (insee) REFERENCES cables74.t_communes(insee);
+UPDATE cables74.t_inventaire_troncons_erdf SET insee = c.insee FROM cables74.t_inventaire_troncons_erdf p JOIN cables74.t_communes c ON p.commune=c.nom_commune;
